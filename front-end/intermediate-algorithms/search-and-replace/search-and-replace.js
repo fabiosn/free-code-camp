8fbbe33b39065ca8b468820a4a71be476af1dcf5
fabio <fabio.queluci@gmail.com>
2017-02-06 13:41:17 -0200

    Take the case of the original word into consideration

--- front-end/intermediate-algorithms/search-and-replace/search-and-replace.js
+++ front-end/intermediate-algorithms/search-and-replace/search-and-replace.js
@@ -1,4 +1,8 @@
 function myReplace(str, before, after) {
+  if (before[0] === before[0].toUpperCase()) {
+    after = after[0].toUpperCase() + after.slice(1);
+  }
+
   return str.replace(before, after);
 }
 
