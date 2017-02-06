2d28344eb2aebc5bab86ad58433349796ce75e70
fabio <fabio.queluci@gmail.com>
2017-02-06 14:01:01 -0200

    Add the first letter plus 'ay' at the end of every word

--- front-end/intermediate-algorithms/pig-latin/pig-latin.js
+++ front-end/intermediate-algorithms/pig-latin/pig-latin.js
@@ -1,5 +1,9 @@
 function translatePigLatin(str) {
+  str = str.slice(1) + str[0] + 'ay';
+
   return str;
 }
 
-translatePigLatin("consonant");
+console.log(translatePigLatin("consonant"));
+console.log(translatePigLatin("california"));
+console.log(translatePigLatin("algorithm"));
