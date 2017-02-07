6f9bf946a25aeeaaf2ad9b88b1497c2e028980c9
fabio <fabio.queluci@gmail.com>
2017-02-07 15:59:24 -0200

    Check if the string is missing a letter and handle the cases when that is false

--- front-end/intermediate-algorithms/missing-letters/missing-letters.js
+++ front-end/intermediate-algorithms/missing-letters/missing-letters.js
@@ -1,5 +1,13 @@
 function fearNotLetter(str) {
-  return str;
+  var charCodeDiff = str.charCodeAt(str.length - 1) - str.charCodeAt(0) + 1;
+
+  if (charCodeDiff - str.length == 0) {
+    return undefined;
+  }
+
+  return 'missing a letter';
 }
 
-fearNotLetter("abce");
+console.log(fearNotLetter("abce"));
+console.log(fearNotLetter("abcdefghjklmno"));
+console.log(fearNotLetter("bcd"));
