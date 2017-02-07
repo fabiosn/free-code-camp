558f54354c875d25ba5d763a39563b99fd8926a9
fabio <fabio.queluci@gmail.com>
2017-02-07 15:12:12 -0200

    Find out the pairing element of each nucleobase provided

--- front-end/intermediate-algorithms/dna-pairing/dna-pairing.js
+++ front-end/intermediate-algorithms/dna-pairing/dna-pairing.js
@@ -1,7 +1,25 @@
 function pairElement(str) {
   var nucleoBases = str.split('');
 
+  for (var i = 0; i < nucleoBases.length; i++) {
+    switch(nucleoBases[i]) {
+      case 'A':
+        console.log('T');
+        break;
+      case 'C':
+        console.log('G');
+        break;
+      case 'G':
+        console.log('C');
+        break;
+      case 'T':
+        console.log('A');
+        break;
+    }
+  }
+
   return nucleoBases;
 }
 
 console.log(pairElement("GCG"));
+console.log(pairElement("ATCGA"));
